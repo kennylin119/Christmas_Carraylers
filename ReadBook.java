7a397a51cd20807cac07f8fe5610e2d16cc141a8
shruthi00 <33496337+shruthi00@users.noreply.github.com>
2018-01-07 16:41:17 -0500

    Added ReadBook.java
    Template for class ReadBook

---
+++ ReadBook.java
@@ -0,0 +1,43 @@
+// Shruthi Venkata, Kenny Lin, Maryann Foley
+// APCS1 pd8
+// Final Project
+// 2018-01-07
+
+import cs1.Keyboard;
+
+public class ReadBook {
+	
+	//instance vars
+	private String book;
+	private int page;
+	//**check this:
+	private static final String[] WORDS = Stats.wordCounter();
+
+	//instead of making these Strings we can just make it void and SOP--evaluate return types for this and other classes
+/* 	public String next() {
+		
+	} */
+	
+/* 	public String back() {
+		
+	} */
+	
+/* 	public String goToPage() {
+		
+	} */
+	
+	//RETURN TYPE? will it return a page number so that the user can check?
+/* 	public String findWord() {
+		
+	} */
+	
+/* 	public void pageMarker() {
+		
+	} */
+
+/* 	public String returnToPage() {
+		
+	}
+ */
+
+} //end class ReadBook
