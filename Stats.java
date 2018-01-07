6a8025ac4969870c3cdb741efb93c3f31695f9af
shruthi00 <33496337+shruthi00@users.noreply.github.com>
2018-01-07 16:33:21 -0500

    Added Stats.java
    Template for class Stats

---
+++ Stats.java
@@ -0,0 +1,49 @@
+// Shruthi Venkata, Kenny Lin, Maryann Foley
+// APCS1 pd8
+// Final Project
+// 2018-01-07
+
+import cs1.Keyboard;
+
+public class Stats {
+
+	//instance vars
+	private String book;
+	private int words;
+	private int sentences;
+	private int userWPM;
+	
+	public Stats() {
+		userWPM = Keyboard.readInt();
+	}
+	
+	/* public String toString() {
+	
+	} */
+	
+/* 	public int wordCounter() {
+	
+	} */
+	
+/* 	public int wordLength() {
+	
+	} */
+	
+/* 	public int sentenceCounter() {
+	
+	} */
+
+/* 	public int wordsPerSentence() {
+	
+	} */
+	
+/* 	public int calcTime() {
+	
+	} */
+
+/* 	public String calcDifficulty() {
+	
+	} */	
+
+
+} //end class Stats
