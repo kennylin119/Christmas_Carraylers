3d0c7b025d5f03626bc9f51fd4cf61fa5cdca644
shruthi00 <33496337+shruthi00@users.noreply.github.com>
2018-01-07 16:27:15 -0500

    Added Library.java
    Template for class Library

---
+++ Library.java
@@ -0,0 +1,76 @@
+// Shruthi Venkata, Kenny Lin, Maryann Foley
+// APCS1 pd8
+// Final Project
+// 2018-01-07
+
+//import java.util.ArrayList;
+import cs1.Keyboard;
+
+public class Library {
+	//instance vars
+	private String[][][] _library;
+	
+	
+	//planned for this to be an ArrayList-- benefits here of ArrayLists v regular arrays?
+	public Library() {
+		//_library = new String[2][10][?];
+	}
+
+/* 	public String printStats() {
+		
+	} */
+
+/* 	public String toString() {
+		
+	} */
+	
+	/* public String enter2d(String subgenre) {
+		
+	} */
+
+	/* public String backTo1d() {
+		
+	} */
+	
+	/* public String enter3d(String title) {
+		
+	} */
+	
+	/* public String backTo2d() {
+		
+	} */
+
+	/* public String printTime() {
+		
+	} */
+	
+	/* public String printStats() {
+		
+	} */
+
+	/* public String searchTitle() {
+		
+	} */
+
+	
+//main method
+/* 	public static void main (String[] args) {
+		Library lib = new Library();
+		System.out.println(lib);
+		String subgenre = Keyboard.readString();
+		lib.enter2d(subgenre);
+		System.out.println(lib);
+		String title = Keyboard.readString();
+		lib.enter3d(title);
+		System.out.println(lib);
+		lib.backTo2d();
+		System.out.println(lib);
+		lib.backTo1d();
+		System.out.println(lib);
+		
+		//by the final version should have a single call to browse library
+		//could refer to hw48 GuessNumber
+	} */
+
+
+} //end class Library
