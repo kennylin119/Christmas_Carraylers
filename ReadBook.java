1d90ad549f00a557acf00a8e08d1117d99ee7ae5
shruthi00 <33496337+shruthi00@users.noreply.github.com>
2018-01-14 02:54:30 -0500

    Updated ReadBook, uml, and devlog

--- ReadBook.java
+++ ReadBook.java
@@ -10,10 +10,27 @@ import java.io.File;
 public class ReadBook {
 	
     //instance vars
-    private String book;
-    private int page;
-    //**check this:
-    //private static final String[] WORDS = Stats.wordCounter();
+	
+	public Scanner _book;
+    private int _page;
+	public int _counter;
+	
+	//constructor
+	
+	public ReadBook(String bookChoice) {
+		
+		try {
+		File text = new File("Genres/Nonfiction/" + bookChoice);
+		_book = new Scanner(text);
+		}
+		
+		catch (Exception FileNotFoundException) {
+			System.out.println("Sorry, file not found.");
+		}
+		
+		_counter = 0;
+		_page = 0;
+	}
 
     //each "page" is a specified number of words that, with average word length, is calculated to fit on a default terminal window
     //instead of making these Strings we can just make it void and SOP--evaluate return types for this and other classes
@@ -23,9 +40,15 @@ public class ReadBook {
 	} */
 
     //changes int page -= 1 so the book "flips" backward
-    /* 	public String back() {
-		
-	} */
+    	public String back() {
+		if (_page==0) {
+			return "You are on the first page.";
+		}
+		else {
+			_page--;
+			return "";
+			}
+		}
 
     //flips  book to specific int page number
     /* 	public String goToPage() {
@@ -52,33 +75,48 @@ public class ReadBook {
 	}
     */
  
-    public static int countWords(String text){
-
-	int counter  = 0;
-	boolean word = false;
-	int endOfLine = text.length() - 1;
-
-	for (int i = 0; i < text.length(); i++) {
-	    // if the char is a letter, word = true.
-	    if (Character.isLetter(text.charAt(i)) && i != endOfLine) {
-		word = true;
-		// if char isn't a letter and there have been letters before,
-		// counter goes up.
-	    } else if (!Character.isLetter(text.charAt(i)) && word) {
-		counter += 1;
-		word = false;
-		// last word of String; if it doesn't end with a non letter, it
-		// wouldn't count without this.
-	    } else if (Character.isLetter(text.charAt(i)) && i == endOfLine) {
-		counter += 1;
-	    }
+/*     public int countWords(String text){
+		boolean word = false;
+		int endOfLine = text.length() - 1;
+		//could we make this neater using Character.isWhitespace() ?
+		for (int i = 0; i < text.length(); i++) {
+			// if the char is a letter, word = true.
+			if (Character.isLetter(text.charAt(i)) && i != endOfLine) {
+				word = true;
+			// if char isn't a letter and there have been letters before,
+			// counter goes up.
+			}
+			else if (!Character.isLetter(text.charAt(i)) && word) {
+				_counter += 1;
+				word = false;
+			// last word of String; if it doesn't end with a non letter, it
+			// wouldn't count without this.
+			}
+			else if (Character.isLetter(text.charAt(i)) && i == endOfLine) {
+				_counter += 1;
+			}
+		}
+		return _counter;
+    } */
+	
+	//likely oversimplified but it works!
+ 	public int wordCounter() {
+		while (_book.hasNext()) {
+			_book.next();
+			//comment out the previous line and uncomment the following line if you would also like to see the text (a temporary fix)
+			//System.out.print(_book.next() + " ");
+			_counter++;
+			}
+		return _counter;
 	}
-	return counter;
-    }
+	
     public static void main (String[] args) {
-	try {File test = new File("Genres/Nonfiction/Paine_CommonSense - short.txt");
-	    Scanner sc = new Scanner(test);
-	    Scanner lineScanner;
+
+		ReadBook blue = new ReadBook("Paine_CommonSense - short.txt");
+		System.out.println();
+		System.out.println(blue.wordCounter());
+		
+	/* Scanner lineScanner;
 	    String text = ""; 
 	    while (sc.hasNext()) {
 		int counter = 0;
@@ -86,19 +124,16 @@ public class ReadBook {
 		lineScanner = new Scanner(nextInputLine);
 		while (lineScanner.hasNextLine())
 		    {
-			counter += 1;
+			_counter += 1;
 			text += lineScanner.next() + " "; //prints out word plus space 
-			if (counter >= 10) { //after ten words add a line break
-			    counter = 0; //temp fix will have to change after we implement sentence parsing
+			if (_counter >= 10) { //after ten words add a line break
+			    _counter = 0; //temp fix will have to change after we implement sentence parsing
 			    text += "\n"; 
 			}
 		    }
 		    }
 	    System.out.println(countWords(text));
-	}
-	catch (Exception FileNotFoundException) {
-	    System.out.println("Sorry, file not found.");
-	}
+	} */
     }
 
 } //end class ReadBook
