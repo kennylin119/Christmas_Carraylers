37fd97aa7d0ca9db612240662dfa277419a89238
shruthi00 <svenkata@stuy.edu>
2018-01-14 15:48:38 -0500

    Modified devlog and Libary

--- Library.java
+++ Library.java
@@ -1,18 +1,25 @@
 // Shruthi Venkata, Kenny Lin, Maryann Foley
 // APCS1 pd8
 // Final Project
-// 2018-01-07
+// 2018-01
 
 //import java.util.ArrayList;
-//import cs1.Keyboard;
+import cs1.Keyboard;
+import java.io.File;
 
 public class Library {
 	//instance vars
-	private String[][][] _library;
+	//private String[][][] _library;
+	private String _input;
+	private String _genre;
+	private String _subgenre;
+	private String _title;
+	private int _wpm;
 	
 	//planned for this to be an ArrayList-- benefits here of ArrayLists v regular arrays?
 	//ArrayLists would likely make it easier to do stats
 	public Library() {
+		//String[][][] = _library = ;
 		//_library = new String[2][10][?];
 	}
  
@@ -26,25 +33,68 @@ public class Library {
 		//Possibly just print title/subgenre/author
 	} */
 
-    //enters second array dimension containing the list of book subgenres
-	/* public String enter2d(String subgenre) {
-		
-	} */
-
     //
-	/* public String backTo1d() {
-		
-	} */
+ 	public String get1d() {
+		File genres = new File("Genres");
+		int i = 1;
+		System.out.println("Pick a genre from the below list by entering the corresponding number and hitting enter.\n");
+		for (String genre : genres.list()) {
+			System.out.println(i + " " + genre);
+			i++;
+		}
+		System.out.println();
+		_input = Keyboard.readString(); //user inputs desired subgenre
+		if (Integer.parseInt(_input)-1 < i) {
+			_genre = genres.list()[Integer.parseInt(_input)-1];
+			return "You chose " + _genre;
+		}
+		else {
+			System.out.println("The number you entered does not correspond to any genre.\n");
+			return get1d();
+		}
+	}
 
-    //enters third array dimendion, containing individual titles of books pertaining to subgenre 
-	/* public String enter3d(String title) {
-		
-	} */
+    //enters third array dimension, containing individual titles of books pertaining to subgenre 
+ 	public String get2d(String genre) {
+		File subgenres = new File("Genres/" + _genre);
+		int i = 1;
+		System.out.println("Pick a subgenre from the below list by entering the corresponding number and hitting enter.\n");
+		for (String subgenre : subgenres.list()) {
+			System.out.println(i + " " + subgenre);
+			i++;
+		}
+		System.out.println();
+		_input = Keyboard.readString(); //user inputs desired subgenre
+		if (Integer.parseInt(_input)-1 < i) {
+			_subgenre = subgenres.list()[Integer.parseInt(_input)-1];
+			return "You chose " + _subgenre;
+		}
+		else {
+			System.out.println("The number you entered does not correspond to any subgenre.\n");
+			return get2d(_genre);
+		}
+	}
 
-    //returns back to list of subgenres
-	/* public String backTo2d() {
-		
-	} */
+    //returns titles
+	public String get3d(String subgenre) {
+		File titles = new File("Genres/" + _genre + "/" + _subgenre);
+		int i = 1;
+		System.out.println("Pick a title from the below list by entering the corresponding number and hitting enter.\n");
+		for (String title : titles.list()) {
+			System.out.println(i + " " + title);
+			i++;
+		}
+		System.out.println();
+		_input = Keyboard.readString(); //user inputs desired book
+		if (Integer.parseInt(_input)-1 < i) {
+			_title = titles.list()[Integer.parseInt(_input)-1];
+			return "You chose " + _title;
+		}
+		else {
+			System.out.println("The number you entered does not correspond to any title.\n");
+			return get3d(_subgenre);
+		}
+	}
 
     //prints time estimated time to read book based on book statistics and user reading speed
     //uses calcTime?
@@ -53,20 +103,22 @@ public class Library {
 	} */
 
     //allows user to input string and checks whether it matches any book titles in the third dimension
-	//will ignore any puncuation or capitalization
+	//will ignore any punctuation or capitalization
     //Should we also have a searchAuthor?
 
 	/* public String searchTitle() {
 		
 	} */
-
 	
 //main method
-/* 	public static void main (String[] args) {
+ 	public static void main (String[] args) {
 		Library lib = new Library();
-		System.out.println(lib); 
+		//lib.pickBook();
+		System.out.println(lib.get1d());
+		System.out.println(lib.get2d(lib._genre));
+		System.out.println(lib.get3d(lib._subgenre));
 
-		System.out.println("What subgenre of books would you like to read?");
+/* 		System.out.println("What subgenre of books would you like to read?");
 		String subgenre = Keyboard.readString(); //user inputs desired subgenre 	
 		lib.enter2d(subgenre); //enters inputed subgenre
 
@@ -78,12 +130,12 @@ public class Library {
 		lib.backTo2d();
 		System.out.println(lib);
 		lib.backTo1d();
-		System.out.println(lib);
+		System.out.println(lib); */
 		
 		//by the final version should have a single call to browse library
 		//ReadBook.main();
 		//could refer to hw48 GuessNumber
-	} */
+	}
 
 
 } //end class Library
