7bf73cde5c67a84b75eb0486b1a3e8aef28cd4cc
shruthi00 <svenkata@stuy.edu>
2018-01-17 06:07:55 -0500

    modified Genres

--- ReadBook.java
+++ ReadBook.java
@@ -10,10 +10,8 @@ import java.io.File;
 public class ReadBook {
 	
     //instance vars
-	
-	private Scanner _book;
-	private Scanner _bookR;
-	private File _text;
+
+	//private File _text;
     private int _page;
 	private int _numPgs;
 	public int _wordCount;
@@ -23,30 +21,27 @@ public class ReadBook {
     private int _avgWL;
     private int _avgSL;
     private String _textStr; //string version of text
+	private String _genre;
+	private String _subgenre;
+	private String _title;
 	
 	//constructor
 	
 	public ReadBook(String genre, String subgenre, String title) {
-		
-		try {
-		_text = new File("Genres/" + genre + "/" + subgenre + "/" + title);
-		_book = new Scanner(_text);
-		_bookR = new Scanner(_text);
-		}
-		
-		catch (Exception FileNotFoundException) {
-			System.out.println("Sorry, file not found.");
-		}
+		_genre = genre;
+		_subgenre = subgenre;
+		_title = title;
 		_wordCount = 0;
 		_page = 0;
 		_numPgs = calcPgs();
+		
 	}
 
     //each "page" is a specified number of words that, with average word length, is calculated to fit on a default terminal window
     //instead of making these Strings we can just make it void and SOP--evaluate return types for this and other classes
     //changes int page += 1 so the book "flips" forward
     public void next() {
-		if (_page>=_numPgs-1) {
+		if (_page>=_numPgs) {
 			System.out.println( "The end - you are on the last page!");
 		}
 		else {
@@ -69,18 +64,19 @@ public class ReadBook {
 		}
 		
 	public int calcPgs() {
-		int x;
-		if (wordCounter()%350 == 0) {
-			x = wordCounter()/350;
+		if (_wordCount%350 == 0) {
+			_numPgs = _wordCount/350;
 		}
 		else {
-			x = wordCounter()/350 + 1;
+			_numPgs = _wordCount/350 + 1;
 		}
-		return x;
+		return _numPgs;
 	}
 		
  	public void printPage(){
- 	    String text = "";
+		try {
+			File txt = new File("Genres/" + _genre + "/" + _subgenre + "/" + _title);
+			Scanner book = new Scanner(txt);
 			int ctrStart = (_page-1)*350;
 			System.out.println("ctrStart is " + ctrStart);
 			int ctrEnd = (_page)*350;
@@ -89,26 +85,28 @@ public class ReadBook {
 		System.out.println(" ");
 		System.out.println("Page "+ _page );
 		System.out.println(" ");
- 	    while (_bookR.hasNext() && ctr < ctrEnd) {
+ 	    while (book.hasNext() && ctr < ctrEnd) {
 			if( ctr < ctrStart ) {
-				_bookR.next();
-				/*_text += _bookR.next();
+				book.next();
+				/*_text += book.next();
 				System.out.println("Hello");
 				System.out.println(_text);*/    
 			}
  			else if( ctr > ctrStart && ctr < ctrEnd ) {
- 				System.out.print(_bookR.next() + " "); //prints out word plus space
+ 				System.out.print(book.next() + " "); //prints out word plus space
  			}
 			ctr+=1;
+		}
+		book.close();}
+		catch (Exception FileNotFoundException) {
+			System.out.println("Sorry, file not found.");
 		}
 
   	}
 
     //flips  book to specific int page number
     public void goToPage(int pg) {
-		
 		if (pg < _numPgs) {
-			
 			_page = pg;
 			printPage();
 		}
@@ -139,40 +137,49 @@ public class ReadBook {
 	
 	//likely oversimplified but it works!
  	public int wordCounter() {
-		while (_book.hasNext()) {
-			_textStr += _book.next();
+		try {
+		File text2 = new File("Genres/" + _genre + "/" + _subgenre + "/" + _title);
+		Scanner book = new Scanner(text2);
+		while (book.hasNext()) {
+			_wordCount++;
+			_textStr += book.next() + " ";
+			/* System.out.println(_wordCount);
+			System.out.println(_textStr); */
 			//System.out.println(_textStr);
 			//comment out the previous line and uncomment the following line if you would also like to see the text (a temporary fix)
 			//System.out.print(_book.next() + " ");
-			_wordCount++;
 			}
-			//_book.close();
-		return _wordCount;
+			book.close();
+			return _wordCount;
+		}
+		catch (Exception FileNotFoundException) {
+			System.out.println("Sorry, file not found.");
+			return 0;
+		}
 	}
-    public int numSentences() {
-     _charCount = 0;
-     _sentenceCount = 0;
-    for (int i = 0; i < _textStr.length() - 1; i++) {
-	String letter = _textStr.substring(i, i += 1);
-	if (letter.matches("[a-zA-Z]+"))// set of all alphabets
-	    {
-		_charCount += 1; //increase only if actual letter
-	    }
-	if (letter.matches("[.?!]"))
-	    {
-		_sentenceCount += 1; //rough estimate of number of sentences
-	    }
-    } return _sentenceCount;
-}
+	
+     public int numSentences() {
+		_charCount = 0;
+		_sentenceCount = 0;
+		for (int i = 0; i < _textStr.length() - 1; i++) {
+			String letter = _textStr.substring(i, i += 1);
+			if (letter.matches("[a-zA-Z]+")) {// set of all alphabets 
+			_charCount += 1; //increase only if actual letter
+			}
+			if (letter.matches("[.?!]")) {
+				_sentenceCount += 1; //rough estimate of number of sentences
+			}
+		}
+		return _sentenceCount;
+	}
 
-public int avgWordLength(){
-     return _avgWL = _charCount/_wordCount; 
-}
+	public int avgWordLength(){
+		 return _avgWL = _charCount/_wordCount; 
+	}
 
-    public int avgSentenceLength()
-    {
-	int charsInSentence = _charCount/_sentenceCount;
-	 return _avgSL = charsInSentence / _avgWL;
-    }
+	public int avgSentenceLength() {
+		int charsInSentence = _charCount/_sentenceCount;
+		 return _avgSL = charsInSentence / _avgWL;
+		}
 
-} //end class ReadBook
+	} //end class ReadBook
